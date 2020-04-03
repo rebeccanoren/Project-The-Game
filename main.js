b778f111f96e84647ca9d08327f45ad2f2b69b71
Rebecca Norén <rebecca.noren@iteam.se>
2020-04-03 17:46:02 +0200

    added slide out

--- main.js
+++ main.js
@@ -1,39 +1,45 @@
-(function () {
+  (function () {
 
-  var bv = new Bideo();
-  bv.init({
-    // Video element
-    videoEl: document.querySelector('#background_video'),
+    var bv = new Bideo();
+    bv.init({
+      // Video element
+      videoEl: document.querySelector('#background_video'),
 
-    // Container element
-    container: document.querySelector('body'),
+      // Container element
+      container: document.querySelector('body'),
 
-    // Resize
-    resize: true,
+      // Resize
+      resize: true,
 
-    // autoplay: false,
+      // autoplay: false,
 
-    isMobile: window.matchMedia('(max-width: 768px)').matches,
+      isMobile: window.matchMedia('(max-width: 768px)').matches,
 
-    playButton: document.querySelector('#play'),
-    pauseButton: document.querySelector('#pause'),
+      playButton: document.querySelector('#play'),
+      pauseButton: document.querySelector('#pause'),
 
-    // Array of objects containing the src and type
-    // of different video formats to add
-    src: [
-      {
-        src: 'night.mp4',
-        type: 'video/mp4'
-      },
-      {
-        src: 'night.webm',
-        type: 'video/webm;codecs="vp8, vorbis"'
+      // Array of objects containing the src and type
+      // of different video formats to add
+      src: [{
+          src: 'night.mp4',
+          type: 'video/mp4'
+        },
+        {
+          src: 'night.webm',
+          type: 'video/webm;codecs="vp8, vorbis"'
+        }
+      ],
+
+      // What to do once video loads (initial frame)
+      onLoad: function () {
+        document.querySelector('#video_cover').style.display = 'none';
       }
-    ],
+    });
+  }());
 
-    // What to do once video loads (initial frame)
-    onLoad: function () {
-      document.querySelector('#video_cover').style.display = 'none';
-    }
-  });
-}());
+  let head = document.querySelector(".head")
+  head.addEventListener("click", fadeOut)
+
+  function fadeOut() {
+    head.classList.add("slide-top")
+  }
